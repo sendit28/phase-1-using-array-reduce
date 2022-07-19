a33b28bafa73d0a10e3ac655101b8ce3e9f09210
+ <+>
2022-07-19 11:03:15 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(total, batches) {
+    return batches + total
+    }
+)
+
+
+
+
 // Code your solution here
+// const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
+// expected output: 10
+
+
+
+
